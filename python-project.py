bfa50f3e2a93388a75236279b453c6c447933e35
Matt Klamo <mjk387@scarletmail.rutgers.edu>
2019-11-13 14:52:10 -0500

    Update python-project.py
    Python Code View 2

--- python-project.py
+++ python-project.py
@@ -4,8 +4,8 @@
 #Using a boolean, if more than half the results are true then they are making a good decision, but if half are false, then they are making a bad descion
 #Mostly the question would consist of True or false questions with some multiple choice so the end can be determined.
 #After they do the quiz, there is going to be a visual at the end to show the amount of accidents happen and the deaths the resulted
-def drunk_accidents(deaths):
-score = 0
+#def drunk_accidents(Quiz):
+    score = 0
     print "Welcome to the drunk driving test!"
     print " "
     print " A.) .08 BAC"
@@ -62,18 +62,52 @@ score = 0
         print "You passed! It  is very important to know what drunk driving could do, and how lethal it is."
     else:
         print "You did not pass! Learning the information about drunk driving is urgent, because one day it will help prevent you from ending the life of someone else"
-return drunk_accidents
 #The end of the code is going to print the statistics of drivers that get into drunk driving accidents
+class Questions:
+     def __init__(self, prompt, answer):
+          self.prompt = prompt
+          self.answer = answer
+#question_prompts is going to take in all the questions for the quiz and display the questions along wiht the answers for this multiple choice quiz
+question_prompts = [
+     "Question 1:What is the legal limit for BAC?\n(a).08 BAC\n(b).05 BAC\n(c)The limit is zero",
+     "Question 2:What month do drunk driving accidents occur the most?\n(a) March\n(b)July\n(c)Decemeber",
+     "Question 3:What type of motorvehicle get into more drunk driving accidents?\n(a) Motorcyle\n(b)Car\n(c)Trucks",
+     "Question 4:Which driving age rangbe causes the most drunk driving accidents?\n(a)16-20\n(b)35-44\n(c)21-34",
+     "Question 5:Which state has the most fatalities due to drunk driving?\n(a)Texas\n(b)Flordia\n(c)California",
+]
+#questions_done is going to go through each prompt of the questions and make sure it has an answer that correlates correctly with the question
+questions_done = [
+     Questions(question_prompts[0],"a"),
+     Questions(question_prompts[1],"b"),
+     Questions(question_prompts[2],"a"),
+     Questions(question_prompts[3],"c"),
+     Questions(question_prompts[4],"a"),
+]
+
+def quiz(questions_done):
+     score = 0
+     for questions in questions_done:
+          answer = raw_input(questions.prompt)
+          if answer == questions.answer:
+               score = score + 1
+     if score >= 4:
+        print("You passed! You got", score, "out of", len(questions_done), "It is very important to know what drunk driving could do, and how lethal it is.")
+     else:
+        print("Oh no!! You failed. You got", score, "out of", len(questions_done), "Learning the information about drunk driving is urgent, because one day it will help prevent you from ending the life of someone else")
+
+quiz(questions_done)
+
 print "For every skull, 1,000 people died in a drunk driving accident:"
 text = ""
 #The range prints the skull 10 times due to the amnoutof drunk driving accidents that happen per 1000
 for fatalties in range(10):
+    print "💀",
 print ""
-print "💀"
 print "For every car, an accident has occured:",
 #The range prints the car 10 times due to the amnoutof drunk driving accidents that happen per 1000
 for accidents in range(40):
     text = text + str(fatalties)
+    print "",
     print "🚘",
 print ""
 print "1 out of 4 accidents are caused due to drunk driving, and as an end result; DEATH",
